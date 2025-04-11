985158baf2fb9de5da2f348f8bb322690e0896eb
Michael Busch <m.busch@beckhoff.com>
2025-04-11 16:32:24 +0200

    Update to version 4.0.7.4, renamed and updated powershel script

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Clone the repository or download as a ZIP.<br/>
 Download the [TF7xxx sample images](https://download.beckhoff.com/download/software/GitHub/TF7xxx/TF7xxx_Sample_Images.zip) as ZIP from our website.<br/>
 First unzip the repository if not cloned, then extract the sample images into the images folder.<br/>
-You can use the PowerShell script (PS C:\TF7xxx_Samples> .\SetTcCOMModuleVersion.ps1 4.0.6.5 or 5.5.6) to set the version of the TcCOM modules (Camera or Filesource object) to a specific version.<br/>
+You can use the PowerShell script (PS C:\TF7xxx_Samples> .\SetVisionVersion.ps1 4.0.7.4 or 5.7.4.0) to set the version of the TcCOM modules (Camera or Filesource object) to a specific version.<br/>
 The PLC library always uses the newest version by default.
 
 # About this repository
---
+++ SetVisionVersion.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [string]$version # new version (4 digits, e.g. "4.0.7.4" or "5.7.4.0")
+)
+
+if([string]::IsNullOrWhiteSpace($version)) { throw "Missing version parameter" }
+$versionCheck = $version -as [version]
+$versionDigits = $version.Split('.').Count
+if($null -eq $versionCheck -or $versionDigits -ne 4) { throw "Invalid version '$version' (exactly 4 digits required)" }
+
+$plcFolder = "$PSScriptRoot\PLC"
+$cppFolder = "$PSScriptRoot\_internal\C++"
+$ba = "Beckhoff Automation GmbH";
+
+Get-ChildItem -Path $plcFolder -Filter *.tsproj -Recurse -File -Name | ForEach-Object {
+    $tsProjPath = "$plcFolder\$_"
+    (Get-Content $tsProjPath) -replace "$ba(\|(TcVision|TcIoGigEVision)\|)(\d+.\d+.\d+.\d+)", "$ba`${1}$version" | Set-Content $tsProjPath
+}
+
+Get-ChildItem -Path $cppFolder -Filter *.tsproj -Recurse -File -Name | ForEach-Object {
+    $tsProjPath = "$cppFolder\$_"
+    (Get-Content $tsProjPath) -replace "$ba(\|(Tc3_Vision|TcVision|TcIoGigEVision)\|)(\d+.\d+.\d+.\d+)", "$ba`${1}$version" | Set-Content $tsProjPath
+}
+
+Get-ChildItem -Path $cppFolder -Filter *.tmc -Recurse -File -Name | ForEach-Object {
+    $tmcPath = "$cppFolder\$_"
+    (Get-Content $tmcPath) -replace "$ba\|Tc3_Vision\|(\d+.\d+.\d+.\d+)", "$ba|Tc3_Vision|$version" | Set-Content $tmcPath
+}
+
+Get-ChildItem -Path $cppFolder -Filter *.props -Recurse -File -Name | ForEach-Object {
+    $propsPath = "$cppFolder\$_"
+    (Get-Content $propsPath) -replace "$ba\\Tc3_Vision\\(\d+.\d+.\d+.\d+)", "$ba\Tc3_Vision\$version" | Set-Content $propsPath
+}
